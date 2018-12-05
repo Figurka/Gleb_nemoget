cd6a19da7371996f00fb86a66d2c9de5df68fe88
Figurka <vanek8898@gmail.com>
2018-12-05 22:56:21 +0300

    skeleton

---
+++ Kalk.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+int main()
+{
+    double a, b;
+    char c;
+    cout << "Input expression like: a /n + /nb/n" << endl;
+    cin >> a;
+    cin >> c;
+    cin >> b;
+    switch (c)
+    {
+    case '+': cout << "develop"<<endl;
+        break;
+    case '-': cout << "develop"<<endl;
+        break;
+    case '/': cout << "develop"<<endl;
+        break;
+    case '*':cout << "develop"<<endl;
+        break;
+    deafult: cout << "Mistake"<<endl;;
+    }
+system(pause);
+return 0;
+}
